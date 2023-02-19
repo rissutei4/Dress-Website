75de98a2690dbcc7216edfaec85070e381f8b12a
Yevhenii Kurinov <87529266+rissutei4@users.noreply.github.com>
2023-02-19 19:29:17 +0100

    Desktop version of main page

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,11 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $(".owl-carousel").owlCarousel({
-        items:1,
+        items: 1,
+        nav:true,
+        navText: [" ", " "],
     });
 });
-
+//Open Burger Menu/Close Menu
 const burgCont = document.querySelector(".burger-container-wrapper");
 const burgIcon = document.querySelector(".burgerIcon");
 const topLine = document.querySelector(".TopLine");
@@ -31,3 +33,26 @@ function toggleMenu() {
 }
 
 burgIcon.addEventListener("click", toggleMenu);
+
+
+langToggler = document.querySelector(".sub-menu").querySelectorAll("li");
+console.log(langToggler);
+langToggler.forEach(element => {
+    element.addEventListener("click", function () {
+        langToggler.forEach(ul => ul.classList.remove("active"))
+        this.classList.add("active");
+    })
+});
+
+
+window.onscroll = function () {
+    let stickyheader = document.querySelector('.header');
+    if (window.pageYOffset > 1) {
+//sticky header
+        stickyheader.classList.add("sticky-nav");
+    } else {
+//normal header
+        stickyheader.classList.remove("sticky-nav");
+    }
+}
+
